5e689450ef6fc1f9c3201b3955551af3a8263378
Tankaman sodari <61791226+chanddavid@users.noreply.github.com>
2025-02-07 22:30:53 +0545

    Update urls.py
    changes form master

--- Account/urls.py
+++ Account/urls.py
@@ -7,7 +7,7 @@ from rest_framework.routers import DefaultRouter
 app_name="account"
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),#for changes
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 ]
